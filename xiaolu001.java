6050ff4d3cc0b2a63d12149c84869d3e361dfea8
xiaolu1331 <2517116501@qq.com>
2020-03-29 11:22:06 +0800

    idea git使用（再次加入）

---
+++ xiaolu001.java
@@ -0,0 +1,10 @@
+package com.example.demo.demooo.demo;
+
+/**
+ * Created by Administrator on 2020/3/29.
+ */
+public class xiaolu001 {
+    public static void main(String[] args) {
+        System.out.println("yyyyyyyyyyyy");
+    }
+}
