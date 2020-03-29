4c424e9c3f37fd54910925d6b302cbcb8de91202
xiaolu1331 <54427153+xiaolu1331@users.noreply.github.com>
2020-03-29 12:41:55 +0800

    Update xiaolu001.java

--- xiaolu001.java
+++ xiaolu001.java
@@ -7,12 +7,5 @@ public class xiaolu001 {
     public static void main(String[] args) {
         System.out.println("yyyyyyyyyyyyxxx");
     }
-    public boolean expire(String name,String num) {
-        System.out.println("ddddd");
-        return true;
-    }
-   /* public boolean expire(String name,String num){
-        System.out.println("rr");
-        return true;
-    }*/
+   
 }
